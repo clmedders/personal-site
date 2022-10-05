3a428f872c89f00495cabf62b3c0ad550f621b06
Christian Medders <christian.medders@bixal.com>
2022-10-04 22:09:28 -0400

    ch..

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   const hamburgerMenu = document.getElementById('hamburger-menu') 
   const siteMenu = document.getElementById('site-menu') 
+  const menuItems = document.querySelectorAll('.navbar-item a, #hamburger-menu')
+
+  console.log(menuItems)
+  
 
   
-  if(hamburgerMenu){
-    hamburgerMenu.addEventListener("click", () => {
-      console.log('open-menu')
-      document.body.classList.toggle('no-scroll')
-      siteMenu.classList.toggle("open-menu")
-    })
+  if(menuItems){
+    for (let i = 0; i < menuItems.length; i++) {
+      menuItems[i].addEventListener("click", () => {
+        console.log('open-menu')
+        document.body.classList.toggle('no-scroll')
+        siteMenu.classList.toggle("open-menu")
+      })
+    }
   }
 })
+
+// for (let i = 0; i < menuItems.length; i++) {
+//   // some code
+// }
+
+// get all links and then when clicked apply the no-scroll turn off 
