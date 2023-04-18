4522972da53eb48776662f74f1e62cca70013bf3
Christian Medders <christian.medders@bixal.com>
2023-04-17 21:06:25 -0400

    made hamburger more simple

---
+++ assets/js/hamburger-menu.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburgerMenu = document.getElementById('hamburger-menu') 
+  const siteMenu = document.getElementById('site-menu') 
+
+    hamburgerMenu.addEventListener('click', () => {
+      siteMenu.classList.toggle("open-menu")
+    })
+  
+})
