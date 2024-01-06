bb677b15ff9fbeed84e16046b55ad542269ee0d7
christian medders <christian.medders@bixal.com>
2024-01-06 12:20:49 -0500

    added eslint and ran fix command

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,33 +1,29 @@
-  // get more ron button 
-  const quoteBtn = document.getElementById('quote-btn')
-  // console.log(moreRon)
+/* eslint-disable no-console */
+// get more ron button
+const quoteBtn = document.getElementById('quote-btn');
+// console.log(moreRon)
 
-  // get the quote box 
-  const quoteBox = document.getElementById('quote-box')
-  // console.log(quote)
+// get the quote box
+const quoteBox = document.getElementById('quote-box');
+// console.log(quote)
 
-  // get the character box
-  const character = document.getElementById('character')
+// get the character box
+const character = document.getElementById('character');
 
-  function fetchQuote() {
-  // fech the data from the end point 
-  fetch('https://officeapi.akashrajpurohit.com/quote/random').then(function (response) {
-
-  if (response.ok){
-    return response.json()
-  }
+function fetchQuote() {
+// fech the data from the end point
+  fetch('https://officeapi.akashrajpurohit.com/quote/random').then((response) => {
+    if (response.ok) {
+      return response.json();
+    }
     throw response.status;
-
-  }).then(function (data) {
-    character.textContent = data['character'];
-    quoteBox.textContent = data['quote'];
-    console.log(data);
-  }).catch(function (error) {
+  }).then((data) => {
+    character.textContent = data.character;
+    quoteBox.textContent = data.quote;
+  }).catch((error) => {
   // There was an error
-  console.warn(error);
+    console.warn(error);
   });
-  }
+}
 
-
-
-quoteBtn.addEventListener('click', fetchQuote)
+quoteBtn.addEventListener('click', fetchQuote);
