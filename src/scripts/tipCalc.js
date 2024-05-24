bbfc5219ae9885f48f4c816d58f6fbc5f9266a40
christian medders <christian.medders@gsa.gov>
2024-05-23 21:23:30 -0400

    adding spilt function

--- src/scripts/tipCalc.js
+++ src/scripts/tipCalc.js
@@ -3,21 +3,35 @@
 // Get the buttons
 const tipAmount = document.querySelectorAll('button');
 const billAmount = document.getElementById('bill');
+const custTip = document.getElementById('custTip');
+const billSpilt = document.getElementById('spilt')
+let percentage;
 
 // Add an event listener to each button using a for loop
 tipAmount.forEach(amount => {
   amount.addEventListener('click', () => {
-    const percentage = amount.getAttribute('data-percentage');
+    percentage = amount.getAttribute('data-percentage');
     const bill = +billAmount.value
     const tipAmount = (bill * percentage) / 100;
     const totalBill = bill + tipAmount;
     document.getElementById('amount').textContent = `$${tipAmount.toFixed(2)}`;
     document.getElementById('total').textContent = `$${totalBill.toFixed(2)}`;
+    
+    billSpilt.addEventListener('input', () => {
+      let spilt = +billSpilt.value;
+      const bill = +billAmount.value
+      const tipAmount = (bill * percentage) / 100 / spilt;
+      const totalBill = (bill + tipAmount) / spilt;
+      // const bill = +billAmount.value
+      document.getElementById('amount').textContent = `$${tipAmount.toFixed(2)}`;
+      document.getElementById('total').textContent = `$${totalBill.toFixed(2)}`;
+    });
+
+
   });
 });
 
 // function for custom tip to run after input
-const custTip = document.getElementById('custTip');
 custTip.addEventListener('input', () => {
   // Run the tip calculation using the custom tip percentage
   const custPercentage = custTip.value;
@@ -26,4 +40,14 @@ custTip.addEventListener('input', () => {
   const totalBill = bill + tipAmount;
   document.getElementById('amount').textContent = `$${tipAmount.toFixed(2)}`;
   document.getElementById('total').textContent = `$${totalBill.toFixed(2)}`;
+
+  billSpilt.addEventListener('input', () => {
+    let spilt = +billSpilt.value;
+    const bill = +billAmount.value
+    const tipAmount = (bill * custPercentage) / 100 / spilt;
+    const totalBill = (bill + tipAmount) / spilt;
+    // const bill = +billAmount.value
+    document.getElementById('amount').textContent = `$${tipAmount.toFixed(2)}`;
+    document.getElementById('total').textContent = `$${totalBill.toFixed(2)}`;
+  });
 });
