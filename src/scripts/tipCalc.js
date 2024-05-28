49547be53c512360292aa827fc788b37bf255336
christian medders <christian.medders@gsa.gov>
2024-05-27 21:15:43 -0400

    Checking something new

--- src/scripts/tipCalc.js
+++ src/scripts/tipCalc.js
@@ -8,46 +8,54 @@ const billSpilt = document.getElementById('spilt')
 let percentage;
 
 // Add an event listener to each button using a for loop
-tipAmount.forEach(amount => {
-  amount.addEventListener('click', () => {
-    percentage = amount.getAttribute('data-percentage');
-    const bill = +billAmount.value
-    const tipAmount = (bill * percentage) / 100;
+
+if (billAmount || custTip >= 0) {
+
+  tipAmount.forEach(amount => {
+    amount.addEventListener('click', () => {
+      percentage = amount.getAttribute('data-percentage');
+      const bill = +billAmount.value
+      const tipAmount = (bill * percentage) / 100;
+      const totalBill = bill + tipAmount;
+      document.getElementById('amount').textContent = `$${tipAmount.toFixed(2)}`;
+      document.getElementById('total').textContent = `$${totalBill.toFixed(2)}`;
+      
+      billSpilt.addEventListener('input', () => {
+        let spilt = +billSpilt.value;
+        const bill = +billAmount.value
+        const tipAmount = (bill * percentage) / 100 / spilt;
+        const totalBill = (bill + tipAmount) / spilt;
+        // const bill = +billAmount.value
+        document.getElementById('amount').textContent = `$${tipAmount.toFixed(2)}`;
+        document.getElementById('total').textContent = `$${totalBill.toFixed(2)}`;
+      });
+  
+  
+    });
+  });
+  
+  // function for custom tip to run after input
+  custTip.addEventListener('input', () => {
+    // Run the tip calculation using the custom tip percentage
+    const custPercentage = custTip.value;
+    const bill = +billAmount.value;
+    const tipAmount = (bill * custPercentage) / 100;
     const totalBill = bill + tipAmount;
     document.getElementById('amount').textContent = `$${tipAmount.toFixed(2)}`;
     document.getElementById('total').textContent = `$${totalBill.toFixed(2)}`;
-    
+  
     billSpilt.addEventListener('input', () => {
       let spilt = +billSpilt.value;
       const bill = +billAmount.value
-      const tipAmount = (bill * percentage) / 100 / spilt;
+      const tipAmount = (bill * custPercentage) / 100 / spilt;
       const totalBill = (bill + tipAmount) / spilt;
       // const bill = +billAmount.value
       document.getElementById('amount').textContent = `$${tipAmount.toFixed(2)}`;
       document.getElementById('total').textContent = `$${totalBill.toFixed(2)}`;
     });
-
-
   });
-});
 
-// function for custom tip to run after input
-custTip.addEventListener('input', () => {
-  // Run the tip calculation using the custom tip percentage
-  const custPercentage = custTip.value;
-  const bill = +billAmount.value;
-  const tipAmount = (bill * custPercentage) / 100;
-  const totalBill = bill + tipAmount;
-  document.getElementById('amount').textContent = `$${tipAmount.toFixed(2)}`;
-  document.getElementById('total').textContent = `$${totalBill.toFixed(2)}`;
+} else {
+  console.log('this didnt work')
+}
 
-  billSpilt.addEventListener('input', () => {
-    let spilt = +billSpilt.value;
-    const bill = +billAmount.value
-    const tipAmount = (bill * custPercentage) / 100 / spilt;
-    const totalBill = (bill + tipAmount) / spilt;
-    // const bill = +billAmount.value
-    document.getElementById('amount').textContent = `$${tipAmount.toFixed(2)}`;
-    document.getElementById('total').textContent = `$${totalBill.toFixed(2)}`;
-  });
-});
