d255e1968a1dffb910c47f119e9241e4a290ad5c
christian medders <christian.medders@gsa.gov>
2024-08-15 21:33:09 -0400

    adding styles and making live on site

--- src/scripts/weather.js
+++ src/scripts/weather.js
@@ -1,48 +1,5 @@
 // /* eslint-disable */
 
-// function fetchWeather() {
-//   document.getElementById("weather-btn").addEventListener("click", () => {
-//     navigator.geolocation.getCurrentPosition(
-//       (position) => {
-//         const url = `https://api.weather.gov/points/${position.coords.latitude},${position.coords.longitude}`;
-//         fetch(url)
-//           .then((response) => response.json())
-//           .then((data) => {
-//             const forecastUrl = data.properties.forecast;
-//             fetch(forecastUrl)
-//               .then((response) => response.json())
-//               .then((forecastData) => {
-//                 const currentWeather = forecastData.properties.periods[0];
-//                 const currentTemp = currentWeather.temperature;
-//                 const highTemp = forecastData.properties.periods[1].temperature;
-//                 const lowTemp = forecastData.properties.periods[2].temperature;
-
-//                 try {
-//                   document.getElementById("weather-output").innerHTML = `Conditions: ${currentWeather.shortForecast}`;
-//                   document.getElementById("current-temp").innerHTML = `Current Temp: ${currentTemp}`;
-//                   document.getElementById("high-temp").innerHTML = `High Temp: ${highTemp}`;
-//                   document.getElementById("low-temp").innerHTML = `Low Temp: ${lowTemp}`;
-//                 } catch (error) {
-//                   console.error(`Error updating DOM: ${error.message}`);
-//                 }
-//               })
-//             .catch((error) => {
-//                 console.error(`Error fetching forecast: ${error.message}`);
-//               });
-//           })
-//         .catch((error) => {
-//             console.error(`Error fetching weather data: ${error.message}`);
-//           });
-//       },
-//       (error) => {
-//         console.error(`Error getting location: ${error.message}`);
-//       }
-//     );
-//   });
-// }
-
-// fetchWeather();
-
 function fetchWeather() {
   document.getElementById("weather-btn").addEventListener("click", () => {
     const loadingMessage = document.getElementById("loading-message");
@@ -68,6 +25,11 @@ function fetchWeather() {
                   document.getElementById("current-temp").innerHTML = `Current Temp: ${currentTemp}`;
                   document.getElementById("high-temp").innerHTML = `High Temp: ${highTemp}`;
                   document.getElementById("low-temp").innerHTML = `Low Temp: ${lowTemp}`;
+
+                  const element = document.getElementById("weather-container");
+
+                  element.classList.add("border-4", "border-blue-950", "rounded-lg");
+
                 } catch (error) {
                   console.error(`Error updating DOM: ${error.message}`);
                 } finally {
